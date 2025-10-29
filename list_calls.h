913eb05f9ba1bac6c9b87861dd5bd8d5a6ea59e2
Dorneles <fernandodorneles95@gmail.com>
2025-10-29 14:43:45 -0300

    Created Services list, and updated list_calls and update_calls

--- list_calls.h
+++ list_calls.h
@@ -9,7 +9,7 @@
 #include "structs.h"
 #include "services.h"
 
-int list_calls(Call *call_list[], int call_list_control, int priority, int status, int select_control, char **logs);
+int list_calls_service(CallService *call_list, int priority, int status, int select_control, char **logs);
 void select_parameter(int *priority, int *status);
 
 #endif // LIST_CALLS_H
